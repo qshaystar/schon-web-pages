145cb1265ff3f6c1283ebbfcb73545edd3f3d23e
Sasha Ye <akomat1103@gmail.com>
2019-11-26 15:42:17 +0800

    add blog dropdown

--- js/action.js
+++ js/action.js
@@ -1,27 +1,66 @@
 $(document).ready(function(){
 
+
+	// mobile toggles
+
+	var init = {
+
+		hideNav: function() {
+			if($('.nav').length>0) {
+				$('.nav').slideUp();
+			}
+		},
+
+		hideAccount: function() {
+			if($('.account').length>0) {
+				$('.account').slideUp();
+			}
+		},
+
+		hideDropdown: function() {
+			if($('.blog-dropdown-menu').length>0) {
+				$('.blog-dropdown-menu').hide();
+			}
+		},
+
+	};
+
+	
+
+
+
+
 	$('.user-toggle').click(function(e) {
 		e.preventDefault();
 		e.stopPropagation();
 
-		if($('.nav').length>0) {
-			$('.nav').slideUp().addClass('.account');
-		}
+		init.hideNav();
 		
-		
+
 		$('.account').slideToggle("slow");
 	});
+
 
 	$('.menu-toggle').click(function(e) {
 		e.preventDefault();
 		e.stopPropagation();
 
-		if($('.account').length>0) {
-			$('.account').slideUp().addClass('.nav');
-		}
+		init.hideDropdown();
+		init.hideAccount();
 		
 		$('.nav').slideToggle("slow");
 	});
+
+
+
+	// dropdown menu
+
+	$('.blog-dropdown-toggle').click(function(e) {
+		$('.blog-dropdown-menu').slideToggle("slow");
+	});
+
+
+	// workspace slider
 
 	 $('.slider').slick({
         dots: true,
@@ -30,18 +69,18 @@ $(document).ready(function(){
 	  	speed: 1000,
 	  	slidesToShow: 2,
 	  	slidesToScroll: 2,
-	  	// autoplay: true,
+	  	autoplay: true,
 
 	  	responsive: [
 	  	   {
-	  	     breakpoint: 420,
+	  	     breakpoint: 430,
 	  	     settings: {
 	  	       slidesToShow: 1,
 	  	       slidesToScroll: 1,
 	  	       infinite: true,
 	  	       dots: true,
 	  	       arrows: false,
-	  	       // autoplay: true,
+	  	       autoplay: true,
 	  	     }
 	  	   }
 	  	]   
